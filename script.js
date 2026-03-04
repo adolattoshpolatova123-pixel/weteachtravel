ad1f5b5bec0cd63181413130437e9bf915ba316c
adolattoshpolatova123-pixel <adolattoshpolatova123@gmail.com>
2026-03-04 11:22:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("lang-en").onclick = function () {
 document.getElementById("lang-uz").addEventListener("click", () => applyLanguage("uz"));
 document.getElementById("lang-en").addEventListener("click", () => applyLanguage("en"));
 
-[3/4/2026 11:21 AM] 🍀: price_domestic_title: "Ichki turizm",
+ price_domestic_title: "Ichki turizm",
 price_samarkand: "Samarqand",
 price_bukhara: "Buxoro",
 price_khiva: "Xiva",
@@ -21,7 +21,7 @@ price_kazakhstan: "Qozog‘iston — Olmaota",
 price_turkey: "Turkiya — Istanbul",
 price_uae: "BAA — Dubay",
 price_korea: "Janubiy Koreya — Seul",
-[3/4/2026 11:21 AM] 🍀: price_domestic_title: "Domestic tourism",
+ price_domestic_title: "Domestic tourism",
 price_samarkand: "Samarkand",
 price_bukhara: "Bukhara",
 price_khiva: "Khiva",
@@ -34,3 +34,4 @@ price_kazakhstan: "Kazakhstan — Almaty",
 price_turkey: "Turkey — Istanbul",
 price_uae: "UAE — Dubai",
 price_korea: "South Korea — Seoul",
+
